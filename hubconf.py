9df76c1073edee018e5f9e10757a9239ca17e592
cytosineXT <63288867+cytosineXT@users.noreply.github.com>
2023-07-13 16:02:50 +0800

    Add files via upload

--- hubconf.py
+++ hubconf.py
@@ -4,88 +4,41 @@ def _create(name, pretrained=True, channels=3, classes=2, autoshape=True, verbos
 
     from pathlib import Path
     from models.common import AutoShape, DetectMultiBackend
-    from models.experimental import attempt_load
     from models.yolo import ClassificationModel, DetectionModel, SegmentationModel
-    from utils.downloads import attempt_download
     from utils.general import LOGGER, ROOT, check_requirements, intersect_dicts, logging
     from utils.torch_utils import select_device
+    from utils.general import (LOGGER, Profile, check_file, check_img_size, check_imshow, check_requirements, colorstr, cv2,
+                           increment_path, non_max_suppression, print_args, scale_boxes, strip_optimizer, xyxy2xywh)
 
     if not verbose:
         LOGGER.setLevel(logging.WARNING)
     check_requirements(ROOT / 'requirements.txt', exclude=('opencv-python', 'tensorboard', 'thop'))
     name = Path(name)
     path = name.with_suffix('.pt') if name.suffix == '' and not name.is_dir() else name  # checkpoint path
+
     try:
         device = select_device(device)
-        if pretrained and channels == 3 and classes == 2:
-            try:
-                model = DetectMultiBackend(path, device=device, fuse=autoshape)  # detection model
-                if autoshape:
-                    if model.pt and isinstance(model.model, ClassificationModel):
-                        LOGGER.warning('WARNING ⚠️ YOLOv5 ClassificationModel is not yet AutoShape compatible. '
-                                       'You must pass torch tensors in BCHW to this model, i.e. shape(1,3,224,224).')
-                    elif model.pt and isinstance(model.model, SegmentationModel):
-                        LOGGER.warning('WARNING ⚠️ YOLOv5 SegmentationModel is not yet AutoShape compatible. '
-                                       'You will not be able to run inference with this model.')
-                    else:
-                        model = AutoShape(model)  # for file/URI/PIL/cv2/np inputs and NMS
-            except Exception:
-                model = attempt_load(path, device=device, fuse=False)  # arbitrary model
-        else:
-            cfg = list((Path(__file__).parent / 'models').rglob(f'{path.stem}.yaml'))[0]  # model.yaml path
-            model = DetectionModel(cfg, channels, classes)  # create model
-            if pretrained:
-                ckpt = torch.load(attempt_download(path), map_location=device)  # load
-                csd = ckpt['model'].float().state_dict()  # checkpoint state_dict as FP32
-                csd = intersect_dicts(csd, model.state_dict(), exclude=['anchors'])  # intersect
-                model.load_state_dict(csd, strict=False)  # load
-                if len(ckpt['model'].names) == classes:
-                    model.names = ckpt['model'].names  # set class names attribute
+        model = DetectMultiBackend(path, device=device, fuse=autoshape)  # detection model 在models/common.py里的model检测模型！ 1 这两行居然缺一不可
+        #⚠️⚠️把这个.py 函数 方法换成别的里的model应该就能实现换方法
+        #不行，common.py里只能处理图片，视频得从其他检测方法里调用
+        model = AutoShape(model)  # for file/URI/PIL/cv2/np inputs and NMS  # detection model 在models/common.py里的model检测模型！ 2这两行居然缺一不可
+       
+        # stride, names, pt = model.stride, model.names, model.pt
+        # imgsz = check_img_size(imgsz, s=stride)  # check image size
+        
+        
+        
+        
         if not verbose:
             LOGGER.setLevel(logging.INFO)  # reset to default
         return model.to(device)
-
     except Exception as e:
         help_url = 'https://github.com/cytosineXT/yolov5_newnewship'
         s = f'{e}. Cache may be out of date, try `force_reload=True` or see {help_url} for help.'
         raise Exception(s) from e
 
-
 def custom(path='path/to/model.pt', autoshape=True, _verbose=True, device=None):
-    # YOLOv5 custom or local model
     return _create(path, autoshape=autoshape, verbose=_verbose, device=device)
-
 
 def yolov5s(pretrained=True, channels=3, classes=2, autoshape=True, _verbose=True, device=None):
     return _create('yolov5s', pretrained, channels, classes, autoshape, _verbose, device)
-
-
-
-if __name__ == '__main__':
-    import argparse
-    from pathlib import Path
-
-    import numpy as np
-    from PIL import Image
-
-    from utils.general import cv2, print_args
-
-    # Argparser
-    parser = argparse.ArgumentParser()
-    parser.add_argument('--model', type=str, default='yolov5s', help='model name')
-    opt = parser.parse_args()
-    print_args(vars(opt))
-
-    # Model
-    model = _create(name=opt.model, pretrained=True, channels=3, classes=2, autoshape=True, verbose=True)
-    # model = custom(path='path/to/model.pt')  # custom
-
-    # Images
-    imgs = ['data/images/zidane.jpg'] 
-
-    # Inference
-    results = model(imgs, size=320)  # batched inference
-
-    # Results
-    results.print()
-    results.save()
